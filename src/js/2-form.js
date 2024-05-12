874a8fef67e43e50ebe257f2da17454e67a15f63
svikam <svikam1@gmail.com>
2024-05-13 02:55:06 +0300

    hw-09

---
+++ src/js/2-form.js
@@ -0,0 +1,37 @@
+'use strict';
+const formData = { email: "", message: "" };
+const storageKey = "feedback-form-state";
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', handleInput);
+populateText();
+
+function handleInput(event) {
+    const key = event.target.name;
+    formData[key] = event.target.value.trim();
+    localStorage.setItem(storageKey, JSON.stringify(formData));
+}
+
+function populateText() {
+    const data = JSON.parse(localStorage.getItem(storageKey));
+    if (!data) {
+        return;
+    }
+    const { email, message } = form.elements;
+    email.value = data.email;
+    message.value = data.message;
+}
+
+form.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+    event.preventDefault();
+    if (!formData.email.trim() || !formData.message.trim()) {
+        alert('Fill please all fields');
+        return;
+    }
+    console.log(formData);
+    event.target.reset();
+    localStorage.removeItem(storageKey);
+    Object.assign(formData, { email: '', message: ''});
+}
+
+
